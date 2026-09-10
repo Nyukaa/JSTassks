70a87c931bbee34cc405cbd0caa7d3141a4149d5
Nyukaa <annashitikova@gmail.com>
2026-09-10 12:45:22 +0300

    Implement map function, Fibonacci generator, and Counter II; allow one function call

--- Task1.js
+++ Task1.js
@@ -47,3 +47,100 @@ let compose = function (functions) {
     return result;
   };
 };
+//2635. Apply Transform Over Each Element in Array
+let map = function (arr, fn) {
+  let returnedArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    //returnedArray[i] = fn(arr[i], i);
+    // Использование push вместо прямой записи по индексу
+    returnedArray.push(fn(arr[i], i));
+  }
+  return returnedArray;
+};
+//2648. Generate Fibonacci Sequence
+let fibGenerator = function* () {
+  //Функция-генератор function* умеет приостанавливать свое выполнение на ключевых словах yield, возвращать промежуточное значение, а затем продолжать работу с того же места.
+  let current = 0;
+  let next = 1;
+
+  while (true) {
+    // Сначала возвращаем текущее число Фибоначчи
+    yield current; //Выдает значение наружу и ставит функцию на паузу
+
+    // Пересчитываем значения для следующего шага
+    // Используем деструктурирующее присваивание, чтобы обновить обе переменные одновременно
+    //let temp = current + next;
+    //current = next;
+    //next = temp;
+    [current, next] = [next, current + next];
+  }
+};
+//2665. Counter II
+let createCounter2 = function (init) {
+  let currentValue = init;
+
+  return {
+    increment() {
+      currentValue++;
+      return currentValue;
+    },
+    decrement() {
+      currentValue--;
+      return currentValue;
+    },
+    reset() {
+      currentValue = init;
+      return currentValue;
+    },
+  };
+};
+// var createCounter = function (init) {
+//   let currentValue = init;
+
+//   return {
+//     increment: () => ++currentValue,
+//     decrement: () => --currentValue,
+//     reset: () => (currentValue = init),
+//   };
+// };
+
+// var createCounter = function (init) {
+//   let currentValue = init;
+
+//   return {
+//     increment() {
+//       return ++currentValue;
+//     },
+//     decrement() {
+//       return --currentValue;
+//     },
+//     reset() {
+//       currentValue = init;
+//       return currentValue;
+//     },
+//   };
+// };
+
+//2666. Allow One Function Call
+let once = function (fn) {
+  let hasBeenCalled = false;
+  let result;
+
+  return function (...args) {
+    if (!hasBeenCalled) {
+      hasBeenCalled = true;
+      result = fn(...args); // вызываем оригинальную функцию с аргументами
+      return result;
+    }
+
+    // Во все последующие разы функция ничего не возвращает,
+    // что в JavaScript автоматически означает return undefined;
+  };
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // undefined, fn не вызывается
+ */
